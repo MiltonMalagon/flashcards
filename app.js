407b588cbe1eb18f3ae1ad3c249a287d3f38567e
Milton Malagón <83915699+MiltonMalagon@users.noreply.github.com>
2025-06-12 18:05:08 -0400

    Added test homepage template.

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`The app is running on localhost: ${port}`);
-});
+
+
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send(`<h1>This is Express!</h1>`);
+  //res.send(`<h1>This is Express!</h1>`);
+  res.render('index');
 });
+
 app.get('/hello', (req, res) => {
   res.send(`<h1>Hi, JavaScript Developer!`);
+});
+
+app.listen(port, () => {
+  console.log(`The app is running on localhost: ${port}`);
 });
