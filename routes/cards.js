c138b6431694f341880f5189046b2d533c6f3f6a
Milton Malagón <83915699+MiltonMalagon@users.noreply.github.com>
2025-06-25 15:05:52 -0400

    Added data for flashcards.

--- routes/cards.js
+++ routes/cards.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const {data} = require('../data/flashcardData.json');
+//const data = require('../.../...').data;
+const {cards} = data;
+//const cards = data.cards;
+
+
+// router.get('/', (req, res) => {
+//   res.render('card', {
+//     prompt: "Who is buried in Grant's tomb?",
+//     //hint: "Think about whose tomb it is."
+//     //colors
+//   });
+//   //res.locals.prompt = "Who is buried in Grant's tomb?";
+//   res.render('card');
+// });
 
 router.get('/', (req, res) => {
   res.render('card', {
-    prompt: "Who is buried in Grant's tomb?",
-    //hint: "Think about whose tomb it is."
-    //colors
+    prompt: cards[0].question,
+    hint: cards[0].hint
   });
-  //res.locals.prompt = "Who is buried in Grant's tomb?";
-  res.render('card');
 });
 
 module.exports = router;
