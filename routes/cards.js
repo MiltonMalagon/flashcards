c79af37c76b783a9c87f19612b3d1ef78dd15b26
Milton Malagón <83915699+MiltonMalagon@users.noreply.github.com>
2025-07-15 16:52:36 -0400

    Added regex testing

--- routes/cards.js
+++ routes/cards.js
@@ -1,65 +1,43 @@
 const express = require('express');
 const router = express.Router();
+
 const {data} = require('../data/flashcardData.json');
-//const data = require('../.../...').data;
 const {cards} = data;
-//const cards = data.cards;
 
-
-// router.get('/', (req, res) => {
-//   res.render('card', {
-//     prompt: "Who is buried in Grant's tomb?",
-//     //hint: "Think about whose tomb it is."
-//     //colors
-//   });
-//   //res.locals.prompt = "Who is buried in Grant's tomb?";
-//   res.render('card');
-// });
-
-// router.get('/:id', (req, res) => {
-//   const {side} = req.query;
-//   const {id} = req.params;
-//   const text = cards[id][side];
-//   const {hint} = cards[id];
-//   const templateData = {text, hint};
-
-//   res.render('card', {
-//     prompt: cards[req.params.id].question,
-//     hint: cards[req.params.id].hint
-//   });
-// });
 
 router.get('/', (req, res) => {
   const numberOfCards = cards.length;
   const flashcardId = Math.floor(Math.random() * numberOfCards);
-  res.redirect(`/cards/${flashcardId}?side=question`);
+  
+  res.redirect(`/cards/${flashcardId}`);
 });
 
 router.get('/:id', (req, res) => {
-  const {side} = req.query;
   const {id} = req.params;
-  
-  // const templateData = {text};
-  // const templateData = {text, hint};
-
-  if (!side) {
-    res.redirect(`/cards/${id}?side=question`);
-  }
-
-  const text = cards[id][side];
+  const {side} = req.query;
   const {hint} = cards[id];
+  const text = cards[id][side];
   const templateData = {id, text};
+  const parsedQuery = req._parsedUrl.query;
+  const regex = /question|answer/;
+
+  if (!side || !regex.test(parsedQuery)) {
+    res.redirect(`/cards/${id}?side=question`);
+    console.dir(req)
+  }  
 
   if (side === 'question') {
-    templateData.hint = hint;
     templateData.sideToShow = 'answer';
-    templateData.sideToShowDisplay = 'Answer'
-  } else if (side === 'answer') {
-    templateData.sideToShow = 'question';
-    templateData.sideToShowDisplay = 'Question';
+    templateData.hint = hint;
+    templateData.sideToShowDisplay = 'Answer';
+    res.render('card', templateData);
   }
   
-  res.render('card', templateData);  
+  if (side === 'answer') {
+    templateData.sideToShow = 'question';
+    templateData.sideToShowDisplay = 'Question';
+    res.render('card', templateData);
+  }
 });
 
 module.exports = router;
