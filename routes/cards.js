e747fc318b05a76e5de325e78b11385d68e0fbe5
Milton Malagón <83915699+MiltonMalagon@users.noreply.github.com>
2025-07-16 11:55:19 -0400

    Added possible headers error fix.

--- routes/cards.js
+++ routes/cards.js
@@ -17,19 +17,31 @@ router.get('/:id', (req, res) => {
   const {side} = req.query;
   const {hint} = cards[id];
   const text = cards[id][side];
-  const templateData = {id, text};
+  const name = req.cookies.username;
   const parsedQuery = req._parsedUrl.query;
   const regex = /question|answer/;
+  const templateData = {id, text, name};
+  
 
   if (!side || !regex.test(parsedQuery)) {
     res.redirect(`/cards/${id}?side=question`);
-    console.dir(req)
   }  
 
   if (side === 'question') {
     templateData.sideToShow = 'answer';
     templateData.hint = hint;
     templateData.sideToShowDisplay = 'Answer';
+
+    // templateData ---
+    // {
+    //   id: '2',
+    //   text: "What is a common ...?",
+    //   name: 'Hannah',
+    //   sideToShow: 'answer',
+    //   hint: 'It has the same abbreviation as "resolution"',
+    //   sideToShowDisplay: 'Answer',
+    //   _locals: [Object: null prototype] {}
+    // }
     res.render('card', templateData);
   }
   
